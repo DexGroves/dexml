34ca39dd1b301c86155a0fc959001ba6d2fc214f
Groves <Declan.Groves@allstate.com>
2016-05-13 09:17:46 -0500

    Implement spline functional for PPR

--- dexml/ppr.py
+++ dexml/ppr.py
@@ -2,3 +2,19 @@
 
 
 import numpy as np
+from scipy.interpolate import UnivariateSpline
+
+
+def get_spline_generator(k, s):
+    """Return a UnivariateSpline function for fixed k and s.
+
+    Returned function will accept x, y and evaluate to the fitted
+    values and the fitted derivative as numpy arrays.
+    """
+
+    def fit_spline(x, y):
+        yhat = UnivariateSpline(x, y, k = k, s = s)(x)
+        dyhat_dx = UnivariateSpline(x, y, k = k, s = s).derivative(1)(x)
+        return yhat, dyhat_dx
+
+    return fit_spline
