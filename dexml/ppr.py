a1705a90f3b96d12c8dd27f63cf8578fa4f4f2ac
Dex Groves <Dex.Groves@gmail.com>
2016-05-15 09:55:13 +0100

    Update functions for M=1 PPR

--- dexml/ppr.py
+++ dexml/ppr.py
@@ -3,21 +3,30 @@
 
 import numpy as np
 from scipy.interpolate import UnivariateSpline
+from dexml.ols import ols
 
 
-def get_spline_generator(k, s):
-    """Return a UnivariateSpline function for fixed k and s.
-
-    Returned function will accept x, y and evaluate to the fitted
-    values and the fitted derivative as numpy arrays.
-    """
-
+def fit_spline_generator(k, s):
     def fit_spline(x, y):
-        yhat = UnivariateSpline(x, y, k=k, s=s)(x)
-        dyhat_dx = UnivariateSpline(x, y, k=k, s=s).derivative(1)(x)
-        return yhat, dyhat_dx
+        return UnivariateSpline(x, y, k=k, s=s)
 
     return fit_spline
+
+
+def update_weights(X, y, w, g):
+    lhs = np.dot(w, X.T)
+    rhs = (y - g(np.dot(w, X.T))) / g.derivative(1)(np.dot(w, X.T))
+    target = lhs + rhs
+
+    weights = g.derivative(1)(np.dot(w, X.T))**2
+
+    w_new = ols(X, target[0], weights[0])
+
+    return np.atleast_2d(w_new)
+
+
+def update_g(X, y, w, fit_spline):
+    return fit_spline(np.dot(w, X.T), y)
 
 
 def initialize_w(M, p):
@@ -31,7 +40,7 @@ def ppr_sose(X, y, w, g):
     yhat_total = np.zeros(len(y))
     for wm in w:
         ridge_vec = np.dot(wm, X)
-        ridge_fn_output = g(y, ridge_vec)[0]
+        ridge_fn_output = g(y, ridge_vec)
         yhat_total += ridge_fn_output
 
     return np.sum((y - yhat_total)**2)
