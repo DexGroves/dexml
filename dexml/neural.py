65d51627b55dc09950789aa702c11064fc4152e9
Groves <Declan.Groves@allstate.com>
2016-05-16 11:31:20 -0500

    Update neural logic. Abstract backprop to new object

--- dexml/neural.py
+++ dexml/neural.py
@@ -12,24 +12,61 @@ class SingleLayeredPerceptron(object):
     def __init__(self,
                  p,
                  M=None,
-                 alpha=None,
-                 beta=None,
-                 activation=np.tanh):
+                 act_fn=np.tanh,
+                 act_fn_deriv=lambda z: 1/np.cosh(z)):
 
-        if alpha is None or beta is None:
-            self.alpha = self.initialize_w(M, p)
-            self.beta = self.initialize_w(M, 1)
-            self.activation = activation
+        self.back_propagator = BackPropagator(p, M, act_fn, act_fn_deriv)
 
-    def pass_forward(self, X):
-        Zm = [self.activation(np.dot(alpha_i, X.T))
-              for i, alpha_i in enumerate(self.alpha)]
 
-        Tk = np.array([Zm[i] * beta_i for i, beta_i in enumerate(self.beta)])
+class BackPropagator(object):
+    """Handle updating the weights of an SLP."""
 
-        return np.sum(Tk, axis = 0)
+    def __init__(self, X, y, p, M, act_fn, act_fn_deriv):
+        self.X = X
+        self.y = y
+
+        self.act_fn = act_fn
+        self.act_fn_deriv = act_fn_deriv
+
+        self.alpha = self.initialize_w(M, p)
+        self.beta = self.initialize_w(M, 1)
+
+    def update_weights(self):
+        fkX = pass_forward(self)
+
+
+    def pass_forward(self):
+        """Perform one forward pass through the network to obtain yhat.
+        Stores intermediate Zk and Tk to avoid recomputation later.
+        """
+        self.Zm = self.get_Zm()
+        self.Tk = self.get_Tk()
+
+        return np.sum(self.Tk, axis = 0)
+
+    def pass_backwards(self, fkX):
+        """Perform one backward pass through network."""
+        self.dki = self.get_dki(fkX)
+        self.smi = self.get_smi()
+
+    def get_Zm(self):
+        return np.array([self.act_fn(np.dot(alpha_i, self.X.T))
+                         for i, alpha_i in enumerate(self.alpha)])
+
+    def get_Tk(self):
+        return np.array([self.Zm[i] * beta_i
+                         for i, beta_i in enumerate(self.beta)])
+
+    def get_dki(self, fkX):
+        BTz = np.dot(self.beta.T, self.Zm)
+        return -2 * (self.y - fkX) * BTz
+
+    def get_smi(self):
+        lhs = self.act_fn_deriv(np.dot(self.alpha, self.X.T))
+        rhs = self.beta * self.dki
+        return lhs * rhs
 
     @staticmethod
     def initialize_w(M, p):
-        """Return the random starting weights."""
+        """Return random starting weights."""
         return np.array([np.random.uniform(-1, 1, p) for i in xrange(M)])
