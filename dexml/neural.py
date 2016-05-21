29e2063a625f20718049a23384054ff3b3137292
Dex Groves <Dex.Groves@gmail.com>
2016-05-21 18:15:16 +0100

    Update neural with help from demystified, remove scratch

--- dexml/neural.py
+++ dexml/neural.py
@@ -1,127 +1,109 @@
-"""Simple backpropagated neural nets."""
+"""Simple backpropagated neural nets.
+With help from https://github.com/stephencwelch/Neural-Networks-Demystified
+"""
 
 
+from scipy import optimize
 import numpy as np
-import itertools
+
+
+class Layer(object):
+    """A fully connected layer of a neural network.
+    Responsible for the input weights and representing its own activity."""
+    def __init__(self, input_layer_size, M):
+        self.W = self.initialise_weights(input_layer_size, M)
+
+    def update_W(self, dW1):
+        self.W = self.W + dW1
+
+    def set_W(self, W):
+        self.W = W
+
+    @staticmethod
+    def initialise_weights(n_lhs, n_rhs):
+        return np.random.rand(n_lhs, n_rhs)
 
 
 class SingleLayeredPerceptron(object):
-    """Fully connected neural net with one hidden layer.
-    Only does regression.
-    """
-
-    def __init__(self,
-                 X,
-                 y,
-                 M=2,
-                 act_fn=None,
-                 act_fn_deriv=None,
-                 gamma=0.01):
-
-        if act_fn is None or act_fn_deriv is None:
-            act_fn=lambda x: 1.0/(1+np.exp(-x))
-            act_fn_deriv=lambda x: (np.exp(-1*x) / ((1+np.exp(-1*x))**2))
-
-        self.bp = BackPropagator(X,
-                                 y,
-                                 M,
-                                 act_fn,
-                                 act_fn_deriv,
-                                 gamma)
-
-    def fit(self, n_iter = 1000):
-        for i in xrange(n_iter):
-            self.bp.update_weights()
-
-    def predict(self, X):
-        Zm = self.bp.get_Zm(self.bp.alpha, X)
-        Tk = self.bp.get_Tk(self.bp.beta, Zm)
-        return Tk[0]
-
-
-class BackPropagator(object):
-    """Handle updating the weights of an SLP."""
-
-    def __init__(self, X, y, M, act_fn, act_fn_deriv, gamma=0.01):
-        self.X = X
-        self.y = y
-
-        self.n = X.shape[1]
-        self.p = X.shape[0]
+    """Fully connected, one-hidden layer neural network."""
+    def __init__(self, p, M):
+        self.p = p
         self.M = M
         self.K = 1
 
-        self.act_fn = act_fn
-        self.act_fn_deriv = act_fn_deriv
-        self.gamma = gamma
+        self.hidden = Layer(p, M)
+        self.output = Layer(M, 1)
 
-        self.alpha = self.initialize_w(n_neurons=M, n_inputs=self.p)
-        self.beta = self.initialize_w(n_neurons=self.K, n_inputs=M)
+        self.sigma = lambda x: 1.0 / (1 + np.exp(-x))
+        self.sigma_prime = lambda x: (np.exp(-1 * x) /
+                                      ((1 + np.exp(-1 * x))**2))
 
-    def update_weights(self):
-        # Forward pass
-        Zm = self.get_Zm(self.alpha, self.X)
-        Tk = self.get_Tk(self.beta, Zm)
-        fkX = Tk   # More activation here later?
+    def forward(self, X):
+        self.z2 = np.dot(X, self.hidden.W)
+        self.a2 = self.sigma(self.z2)
 
-        self.error = (self.y - fkX)**2
+        self.z3 = np.dot(self.a2, self.output.W)
+        self.a3 = self.sigma(self.z3)
 
-        # Backwards pass
-        dki = self.get_dki(fkX, Zm)
-        smi = self.get_smi(dki, self.alpha, self.beta)
+        return self.a3
 
-        # Gradients
-        alpha_grad = self.get_alpha_gradient(smi, self.X)
-        beta_grad = self.get_beta_gradient(dki, Zm)
+    def backward(self, X, y):
+        delta3 = np.multiply(-(y - self.z3), self.sigma_prime(self.z3))
+        self.dJdW2 = np.dot(self.a2.T, delta3)
 
-        # Update weights
-        self.beta = self.beta - (self.gamma * np.sum(beta_grad, axis=0))
-        self.alpha = self.alpha - (self.gamma * np.sum(alpha_grad, axis=0))
+        delta2 = np.dot(delta3, self.output.W.T) * self.sigma_prime(self.z2)
+        self.dJdW1 = np.dot(X.T, delta2)
 
-    def get_Zm(self, alpha, X):
-        return self.act_fn(np.dot(alpha, X))
+        return self.dJdW1, self.dJdW2
 
-    def get_Tk(self, beta, Zm):
-        return np.dot(beta, Zm)
+    def get_weights(self):
+        weights = np.concatenate((self.hidden.W.ravel(),
+                                  self.output.W.ravel()))
+        return weights
 
-    def get_dki(self, fkX, Zm):
-        dki = -2 * (self.y - fkX)
-        return dki
+    def get_gradients(self, X, y):
+        dJdW1, dJdW2 = self.backward(X, y)
+        return np.concatenate((self.dJdW1.ravel(), self.dJdW2.ravel()))
 
-    def get_smi(self, dki, alpha, beta):
-        smi = []
-        for i in xrange(self.n):
-            sm = []
-            for m, alpha_m in enumerate(alpha):
-                aTx = np.dot(alpha_m, self.X[:,i])
-                lhs = self.act_fn_deriv(aTx)
+    def set_weights(self, weights):
+        alpha_start = 0
+        alpha_end = self.M * self.p
+        self.hidden.set_W(np.reshape(
+            weights[alpha_start:alpha_end], (self.p, self.M)))
+        beta_end = alpha_end + self.M * self.K
+        self.output.set_W(np.reshape(
+            weights[alpha_end:beta_end], (self.M, self.K)))
 
-                rhs = []
-                for k, beta_k in enumerate(beta):
-                    bTd = (beta_k[m] * dki[k][i])
-                    rhs.append(bTd)
-                rhs = np.sum(rhs)
 
-                sm.append(lhs * rhs)
-            smi.append(np.array(sm))
+class BFGSTrainer(object):
+    """Train an SLP with BFGS."""
+    def __init__(self, slp):
+        self.slp = slp
 
-        return np.array(smi)
+    def fit(self, X, y, method='BFGS'):
+        weights0 = self.slp.get_weights()
 
-    def get_alpha_gradient(self, smi, X):
-        alpha_grad = np.zeros((self.n, self.M, self.p))
-        for i, m, l in itertools.product(
-            xrange(self.n), xrange(self.M), xrange(1, self.p)):
-            alpha_grad[i, m, l] = smi[i][m] * X[l, i]
-        return alpha_grad
+        options = {'maxiter': 200, 'disp': True}
 
-    def get_beta_gradient(self, dki, Zm):
-        beta_grad = np.zeros((self.n, self.K, self.M))
-        for i, k, m in itertools.product(
-            xrange(self.n), xrange(self.K), xrange(1, self.M)):
-            beta_grad[i, k, m] = dki[k][i] * Zm[m][i]
-        return beta_grad
+        _res = optimize.minimize(self.cost_and_grad,
+                                 weights0,
+                                 jac=True,
+                                 method=method,
+                                 args=(X, y),
+                                 options=options)
 
-    @staticmethod
-    def initialize_w(n_neurons, n_inputs):
-        return np.array([np.random.uniform(-0.1, 0.1, n_inputs)
-                         for i in xrange(n_neurons)])
+        self.slp.set_weights(_res.x)
+
+        return _res
+
+    def cost_function(self, X, y):
+        yhat = self.slp.forward(X)
+        return np.sum((y - yhat)**2)
+
+    def cost_and_grad(self, weights, X, y):
+        self.slp.set_weights(weights)
+
+        cost = self.cost_function(X, y)
+        grad = self.slp.get_gradients(X, y)
+
+        return cost, grad
