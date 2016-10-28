31df01973463cabf887057d921870874c4208a46
Groves <Declan.Groves@allstate.com>
2016-10-28 05:17:37 -0500

    Make top level methods importable

---
+++ dexml/__init__.py
@@ -0,0 +1,5 @@
+from dexml.cart import GaussCART
+from dexml.kmeans import KMeans
+from dexml.mlp import NeuralNetwork, BFGSTrainer
+from dexml.ols import ols
+from dexml.ppr import ProjectionPursuitRegressor
